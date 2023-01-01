ae8f542813b26cf4955201bdb5143cecc7627af2
RajatVijaywargiya <rajat.vijaywargiya@gmail.com>
2023-01-02 01:32:11 +0530

    added base test

--- src/test/java/com/tests/BaseTest.java
+++ src/test/java/com/tests/BaseTest.java
@@ -1,6 +1,22 @@
 package com.tests;
 
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import static io.restassured.RestAssured.given;
+
 public class BaseTest {
+
+    @BeforeMethod
+    public void setUp() {
+        
+    }
+
+    @AfterMethod
+    public void tearDown() {
+
+    }
 
 
 }
